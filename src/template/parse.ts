27bdcfaa578c71343d9a2a28bf988530413af587
Nathan Wright <natiouswright@gmail.com>
2023-12-07 15:22:49 -0600

    Setup template and start day 7

---
+++ src/template/parse.ts
@@ -0,0 +1,11 @@
+import { readFileSync } from 'node:fs'
+const testFile = './test.txt'
+const dataFile = './data.txt'
+
+const data = readFileSync(dataFile, 'utf8').split("\n").filter((line) => { line !== "" })
+const test = readFileSync(testFile, 'utf8').split("\n").filter((line) => { line !== "" })
+
+const input = data
+console.log(input)
+
+
