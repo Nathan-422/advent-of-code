7c7e1f1c854dd0dc0be041def664c29c51632743
Nathan Wright <natiouswright@gmail.com>
2023-12-08 18:10:07 -0600

    Fixed error in template

--- src/template/parse.ts
+++ src/template/parse.ts
@@ -2,8 +2,8 @@ import { readFileSync } from 'node:fs'
 const testFile = './test.txt'
 const dataFile = './data.txt'
 
-const data = readFileSync(dataFile, 'utf8').split("\n").filter((line) => { line !== "" })
-const test = readFileSync(testFile, 'utf8').split("\n").filter((line) => { line !== "" })
+const data = readFileSync(dataFile, 'utf8').split("\n").filter((line) => line !== "")
+const test = readFileSync(testFile, 'utf8').split("\n").filter((line) => line !== "")
 
 const input = data
 console.log(input)
