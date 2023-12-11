abd27dc3da3b091122a7695a112eb2fb0137a918
Nathan Wright <natiouswright@gmail.com>
2023-12-11 17:33:08 -0600

    Tweated template to start on test data

--- src/template/parse.ts
+++ src/template/parse.ts
@@ -5,7 +5,7 @@ const dataFile = './data.txt'
 const data = readFileSync(dataFile, 'utf8').split("\n").filter((line) => line !== "")
 const test = readFileSync(testFile, 'utf8').split("\n").filter((line) => line !== "")
 
-const input = data
+const input = test
 console.log(input)
 
 
