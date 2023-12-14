94534182f6e0811a3d7f8d2d5089aaec4aa7ae49
Nathan Wright <natiouswright@gmail.com>
2023-12-13 23:05:44 -0600

    Day 13 part 2 complete

--- src/Day 13 - Point of Incidence/parse.ts
+++ src/Day 13 - Point of Incidence/parse.ts
@@ -8,92 +8,128 @@ enum Rotation {
 }
 
 type Reflection = {
+	field: string[]
 	orientation: Rotation
-	width: number
+	transformationMap: boolean[][]
 	l: number
 	r: number
 }
 
-const isSymetric = (line: string, a: number, b: number): boolean => {
-	return line.at(a) === line.at(b)
+const flipSpace = (space: string[]): string[] => {
+	const remapped: string[][] = []
+	for (let y = 0; y < space[0].length; y++) {
+		remapped.push([])
+		for (let x = 0; x < space.length; x++) {
+			remapped[y].push(space[x][y])
+			// console.log(space[x][y])
+		}
+	}
+	return remapped.map((line) => { return line.join('') })
 }
 
-const isFullReflection = (line: string, center: number[]) => {
+const findSingleInperfections = (field: string[], center: number[]): boolean[][] | null => {
 	const [l, r] = [...center]
-	for (let i = 0; l - i >= 0 && r + i < line.length; i++) {
-		if (!isSymetric(line, l - i, r + i)) {
-			return false
-		}
+
+	const leftLinesToEdge = l + 1
+	const rightLinesToEdge = field[0].length - r
+
+	// get the distance to the edge, essentially
+	const linesToCompare = leftLinesToEdge > rightLinesToEdge ? rightLinesToEdge : leftLinesToEdge
+
+
+	const leftSide: boolean[][] = field
+		.map((line) => {
+			return line
+				.split('')
+				.slice(l - linesToCompare + 1, l + 1)
+				.map((char) => {
+					if (char === '.') return false
+					return true
+				})
+		})
+
+	const rightSide: boolean[][] = field
+		.map((line) => {
+			return line
+				.split('')
+				.slice(r, r + linesToCompare)
+				.reverse()
+				.map((char) => {
+					if (char === '.') return false
+					return true
+				})
+		})
+
+	const halfTransformationArray = leftSide.map((line, i) => {
+		return line.map((bool, j) => {
+			return bool !== rightSide[i][j]
+		})
+	})
+
+	const transformationArray = halfTransformationArray
+		.map(line => {
+			const reversed = line.slice().reverse()
+			return [...line, ...reversed]
+		})
+
+	const count = transformationArray.reduce((count, line) => {
+		return count + line.reduce((lineCount, bool) => {
+			if (bool) {
+				return lineCount + 1
+			}
+			return lineCount
+		}, 0)
+	}, 0)
+
+	if (count === 2) {
+		return transformationArray
 	}
-	return true
+
+	return null
 }
 
-const findReflectionsInLine = (line: string, toCheck: number[][]) => {
-	const symetricLines: number[][] = []
 
-	for (let pair of toCheck) {
-		if (isSymetric(line, pair[0], pair[1])) {
-			symetricLines.push(pair)
-		}
-	}
+const findReflection = (reflection: Reflection): Reflection => {
+	const space = reflection.field
 
-	return symetricLines
-}
-
-const findReflection = (space: string[], isHorizontal: boolean = false): Reflection => {
+	// get all possible pairs for this space
 	let possiblePairs: number[][] = []
-	// console.log(space)
-
-	for (let i = 0; i < space[0].length - 1; i++) {
+	for (let i = 0; i < reflection.field[0].length - 1; i++) {
 		possiblePairs[i] = [i, i + 1]
 	}
 
-	for (let line of space) {
-		possiblePairs = findReflectionsInLine(line, possiblePairs)
-		if (possiblePairs.length === 0) break
-	}
-
-	// if (possiblePairs.length > 1) {
-	// 	console.log((('*').repeat(40) + '\n').repeat(20))
-	// 	console.log('MULTIPLE REFLECTIONS FOUND')
-	// 	console.log(possiblePairs)
-	// }
-
-	const fullWidthPairs: number[][] = []
+	const reflectionPairs: number[][] = []
+	let transformationMap: boolean[][] = []
 	for (let pair of possiblePairs) {
-		let isFullWidth = true
-		for (let line of space) {
-			if (!isFullReflection(line, pair)) {
-				isFullWidth = false
-				break
-			}
+		const transformationMap = findSingleInperfections(space, pair)
+
+		if (transformationMap === null) {
+			continue
 		}
-		isFullWidth ? fullWidthPairs.push(pair) : null
+
+		reflectionPairs.push(pair)
 	}
 
-	let reflection: Reflection
-
-	if (fullWidthPairs.length === 0) {
-		// console.log("Had to flip")
-		return reflection = findReflection(flipSpace(space), true)
+	if (reflectionPairs.length === 0) {
+		return findReflection({
+			...reflection,
+			field: flipSpace(space),
+			orientation: Rotation.h
+		})
 	}
 
-	const spaceLeft = fullWidthPairs[0][0] + 1
-	const spaceRight = space[0].length - fullWidthPairs[0][1]
 
-	reflection = {
-		orientation: isHorizontal ? Rotation.h : Rotation.v,
-		width: spaceLeft > spaceRight ? spaceRight : spaceLeft,
-		l: fullWidthPairs[0][0],
-		r: fullWidthPairs[0][1],
+	return {
+		...reflection,
+		transformationMap: transformationMap,
+		l: reflectionPairs[0][0],
+		r: reflectionPairs[0][1]
 	}
-
-	return reflection
 }
 
-const flipSpace = (space: string[]): string[] => {
-	return space.map((_, colIndex) => space.map(row => row.at(colIndex)).join(''))
-}
+
+
+
 
 const data = readFileSync(dataFile, 'utf8').split("\n\n").filter((line) => line !== "")
 const test = readFileSync(testFile, 'utf8').split("\n\n").filter((line) => line !== "")
@@ -106,7 +142,13 @@ const spaces = input.map(line => {
 const reflections: Reflection[] = []
 for (let space of spaces) {
 	// console.log(space)
-	reflections.push(findReflection(space))
+	reflections.push(findReflection({
+		field: space,
+		orientation: Rotation.v,
+		transformationMap: [],
+		l: 0,
+		r: 0,
+	}))
 }
 
 console.log(reflections.reduce((total, current) => {
@@ -116,6 +158,3 @@ console.log(reflections.reduce((total, current) => {
 
 	return total + ((current.l + 1) * 100)
 }, 0))
-
-// const testLine = '..##..###'
-// console.log(isFullReflection(testLine, [6, 7]))
