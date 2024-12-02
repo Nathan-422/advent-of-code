b7c647089aadf2eb8348748730a59afab6fdeab6
Nathan Wright <natiouswright@gmail.com>
2024-12-01 20:45:25 -0600

    Day 1 part 1 complete

--- 2024/Day 01 - Historian Hysteria/main.py
+++ 2024/Day 01 - Historian Hysteria/main.py
@@ -1,1 +1,24 @@
-open("./data.txt", "r")
+left_list = []
+right_list = []
+
+with open("./data.txt", "r") as file:
+
+    for line in file:
+
+        values = line.rstrip("\n").split("   ")
+        left = int(values[0])
+        right = int(values[1])
+
+        left_list.append(left)
+        right_list.append(right)
+
+left_list.sort()
+right_list.sort()
+
+sum = 0
+
+for i in range(len(left_list)):
+
+    sum += abs(left_list[i] - right_list[i])
+
+print(sum)
