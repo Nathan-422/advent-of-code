58fb986233608c6b5d32420e554034a8eb00c695
Nathan Wright <natiouswright@gmail.com>
2024-12-03 19:25:11 -0600

    Part 1 complete

--- 2024/Day 02 - Red-Nosed Reports/part1.py
+++ 2024/Day 02 - Red-Nosed Reports/part1.py
@@ -1,25 +1,41 @@
-import copy
-
-
-def isIncreasing(line):
-    ordered = sorted(copy.deepcopy(line))
-
-    return line[0] == ordered[0]
-
-
 def getDifferences(line):
     difference = []
     for i in range(len(line)):
         if i == len(line) - 1:
             return difference
-        difference.append(abs(int(line[i]) - int(line[i + 1])))
+        difference.append(int(line[i]) - int(line[i + 1]))
+    return difference
 
 
-with open('test.txt', 'r') as file:
+def isValid(line):
+    direction = 'neutral'
+
+    for n in line:
+        match direction:
+            case 'neutral':
+                if (n > 0 and n <= 3):
+                    direction = 'increasing'
+                elif (n < 0 and n >= -3):
+                    direction = 'decreasing'
+                else:
+                    return False
+            case 'increasing':
+                if (n < 1 or n > 3):
+                    return False
+            case 'decreasing':
+                if (n < -3 or n > -1):
+                    return False
+    return True
+
+
+with open('data.txt', 'r') as file:
+    safe_lines = 0
     for raw_line in file:
         line = [int(x) for x in raw_line.rstrip('\n').split(' ')]
-        print(isIncreasing(line))
-        print(line)
-        print(getDifferences(line))
-        # for i in range(len(line)):
-        #     i + 1
+        diffs = getDifferences(line)
+
+        if (isValid(diffs)):
+            safe_lines += 1
+
+        # print("% s is % s" % (line, 'Safe' if isValid(diffs) else 'UNsafe'))
+    print(safe_lines)
