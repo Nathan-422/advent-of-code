312cb6ddb9dc97b980ff44888433f6d1398a4b38
Nathan Wright <natiouswright@gmail.com>
2024-12-03 21:29:48 -0600

    Start of day 2 part 2

---
+++ 2024/Day 02 - Red-Nosed Reports/part2.py
@@ -0,0 +1,52 @@
+import copy
+
+
+def getDifferences(line):
+    difference = []
+    for i in range(len(line)):
+        if i == len(line) - 1:
+            return difference
+        difference.append(int(line[i]) - int(line[i + 1]))
+    return difference
+
+
+def isValid(line):
+    direction = 'neutral'
+
+    for n in line:
+        match direction:
+            case 'neutral':
+                if (n > 0 and n <= 3):
+                    direction = 'increasing'
+                elif (n < 0 and n >= -3):
+                    direction = 'decreasing'
+                else:
+                    return False
+            case 'increasing':
+                if (n < 1 or n > 3):
+                    return False
+            case 'decreasing':
+                if (n < -3 or n > -1):
+                    return False
+    return True
+
+
+def isPermutationValid(line):
+    for n in range(len(line)):
+        modified = copy.deepcopy(line).pop(n)
+        if (isValid(getDifferences(modified))):
+            return True
+    return False
+
+
+with open('test.txt', 'r') as file:
+    safe_lines = 0
+    for raw_line in file:
+        line = [int(x) for x in raw_line.rstrip('\n').split(' ')]
+        diffs = getDifferences(line)
+
+        if (isValid(diffs) or isPermutationValid(line)):
+            safe_lines += 1
+
+        # print("% s is % s" % (line, 'Safe' if isValid(diffs) else 'UNsafe'))
+    print(safe_lines)
