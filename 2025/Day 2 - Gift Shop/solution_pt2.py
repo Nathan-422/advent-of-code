c1518728d36935bfb9d144b3eb52f68c5c3ad38b
Nathan Wright <natiouswright@gmail.com>
2025-12-17 15:05:50 -0600

    Add function for getting factors of a number

--- 2025/Day 2 - Gift Shop/solution_pt2.py
+++ 2025/Day 2 - Gift Shop/solution_pt2.py
@@ -1,3 +1,14 @@
+from functools import reduce
+
+
+def factors(n: int):
+    return set(
+        reduce(
+            list.__add__, ([i, n // i] for i in range(1, int(n**0.5) + 1) if n % i == 0)
+        )
+    )
+
+
 file = "data.txt"
 file = "example.txt"
 
